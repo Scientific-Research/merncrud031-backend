c72298d0a5ba12a3a75520389c5a7387b4f4c7cb
Maximilian <scientificresearch309@gmail.com>
2023-06-11 14:08:47 +0200

    testing login with bcrypt!

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import * as config from './config.js';
 import { INewBook } from './interfaces.js';
 import session from 'express-session';
 import cookieParser from 'cookie-parser';
+import * as tools from './tools.js';
 
 declare module 'express-session' {
 	export interface SessionData {
@@ -52,10 +53,10 @@ app.get('/book/:id', async (req, res) => {
 	res.status(200).json(book);
 });
 
-app.post('/login', (req: express.Request, res: express.Response) => {
-	const { username, password } = req.body;																
-	const user = model.getUser(username, password);
-	if (password === config.ADMIN_PASSWORD) {
+app.post('/login', async (req: express.Request, res: express.Response) => {
+	const { username, password } = req.body;
+	const user = await model.getUser(username, password);
+	if (tools.passwordIsCorrect(password, user.hash)) {
 		req.session.user = user as any;
 		req.session.cookie.expires = new Date(
 			Date.now() + config.SECONDS_TILL_SESSION_TIMEOUT * 1000
